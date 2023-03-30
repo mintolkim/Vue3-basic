5763ec297371d66217634f62bae738e2919cf724
mintolkim <juna216@naver.com>
2023-03-30 22:15:09 +0900

    7강까지

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>{{name}}</div>
+  <div class="name">{{ name }}</div>
+  <button class="btn btn-primary" v-on:click="updateName">click</button>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
     setup(){
-        const name = '민톨';
-        return name;
+        const name = ref('민톨');
+        /*
+        const greeting = (name) => {
+            return name + 'hi';
+        }
+
+        const greet = greeting(name);
+        */
+
+        const updateName = () => {
+            name.value = '준톨'; // 일반 변수 사용시 값은 바뀌나 template 에선 바뀌지 않음
+            alert("안뇽");
+        }
+        return {
+            name,
+            updateName
+        };
     }
 }
 </script>
+
+<style>
+ .name{
+     color: blue;
+ }
+</style>
