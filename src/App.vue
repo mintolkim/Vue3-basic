217cfbcce073944bb56f9b5b5ee6f2dfe9710848
mintolkim <juna216@naver.com>
2023-04-02 19:11:01 +0900

    9강

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="name">{{ name }}</div>
-  <button class="btn btn-primary" v-on:click="updateName">click</button>
+    <div class="name" :class="nameClass" >{{ name }}</div>
+    <input :type="type" v-model="name">
+    <button class="btn btn-primary" @click="updateName">click</button>
 </template>
 
 <script>
@@ -8,7 +9,9 @@ import { ref } from "vue";
 export default {
     setup(){
         const name = ref('민톨');
-        /*
+        const type = ref('text');
+        const nameClass = ref('name');
+        /*const type = ref('checkbox');
         const greeting = (name) => {
             return name + 'hi';
         }
@@ -17,12 +20,22 @@ export default {
         */
 
         const updateName = () => {
-            name.value = '준톨'; // 일반 변수 사용시 값은 바뀌나 template 에선 바뀌지 않음
-            alert("안뇽");
+            //type.value = 'text';
+            //name.value = '준톨'; // 일반 변수 사용시 값은 바뀌나 template 에선 바뀌지 않음
+            //alert("안뇽");
+            console.log(name.value);
         }
+
+        const onSubmit = (e) => {
+            name.value = e.target.value;
+        }
+
         return {
             name,
-            updateName
+            updateName,
+            onSubmit,
+            nameClass,
+            type
         };
     }
 }
